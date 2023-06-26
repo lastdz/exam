b30f335a13a21a21e7c7180f37a252752057f303
lastxuansss <2393046318@qq.com>
2023-06-26 19:23:41 +0800

    11

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 
 	InitRouter(r)
 
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":7996") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
